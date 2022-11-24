d77f1239834bd565da5168b0e94e9301539622b1
João Ramos <joaoramos@Joaos-MacBook-Pro-3.local>
2022-11-24 22:10:00 +0000

    examples

--- consumer.py
+++ consumer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('my-topic',
-                         bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer('my-topic', bootstrap_servers=['localhost:9092'])
+
 for message in consumer:
     print(message.key)
     print(message.value)
--- producer.py
+++ producer.py
@@ -2,6 +2,6 @@ from kafka import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
-producer.send('my-topic', b'raw_bytes')
+producer.send('my-topic', b'ola')
 
 producer.flush()
